9cbc9a81a3fef628f42119b51aab54b475d6dd11
Jashan Mann <jashan@Jashans-MacBook-Pro-2.local>
2025-01-21 11:51:03 -0800

    added libraries and pip installed yfinance, pandas, numpy, etc

---
+++ stock price predictor/main.py
@@ -0,0 +1,8 @@
+import yfinance
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+import tensorflow as tf 
+
+print("Libraries installed and working!")
